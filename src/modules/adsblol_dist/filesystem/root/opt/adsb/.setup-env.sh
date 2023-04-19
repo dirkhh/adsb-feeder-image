3e16ba88d3faf9382efedd39bed845db6ad2fc68
Dirk Hohndel <dirk@hohndel.org>
2023-04-19 23:47:39 +0100

    Start with a faurly simple setup
    Install docker using a slightly modified version of the docker-install script
    (making it non-interactive and removing some of the print out since no one
    will ever see it).
    
    And create a skeleton for a setup web app (non functional).
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

---
+++ src/modules/adsblol_dist/filesystem/root/opt/adsb/.setup-env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+WORKING_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
+WORKING_DIR="$(cd -- "$WORKING_DIR" && pwd)"
+if [ ! -f "$WORKING_DIR"/.env ] && [ -f /boot/adsblol.env ]; then
+	cp /boot/adsblol.env "$WORKING_DIR"/.env
+fi
+
+if grep -q 00000000-0000-0000-0000-000000000000 "$WORKING_DIR"/.env ; then
+	sed -i "s|^.*FEEDER_UUID=00000000-0000-0000-0000-000000000000|FEEDER_UUID=$(cat /proc/sys/kernel/random/uuid)|" "$WORKING_DIR"/.env
+fi
