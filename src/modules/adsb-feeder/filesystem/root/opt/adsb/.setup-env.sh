bd8a6c30418d5ac735cf477776b69a32bdf4788e
Dirk Hohndel <dirk@hohndel.org>
2023-04-25 05:08:20 +0000

    Switch to a single .env file and fix some bugs
    This way it's easier to drop in a different docker-compose file for
    advanced users. Eventually there should be a UI to do so.
    
    Also this updates to the new syntax of configuring Ultrafeeder.
    And it fixes a bug in the way the feeder settings were parsed from the
    web form.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/.setup-env.sh
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/.setup-env.sh
@@ -2,10 +2,6 @@
 
 WORKING_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
 WORKING_DIR="$(cd -- "$WORKING_DIR" && pwd)"
-if [ ! -f "$WORKING_DIR"/.env ] && [ -f /boot/adsblol.env ]; then
-	cp /boot/adsblol.env "$WORKING_DIR"/.env
-fi
-
 if grep -q 00000000-0000-0000-0000-000000000000 "$WORKING_DIR"/.env ; then
 	sed -i "s|^.*UUID=00000000-0000-0000-0000-000000000000|UUID=$(cat /proc/sys/kernel/random/uuid)|" "$WORKING_DIR"/.env
 fi
