718851fdcf5fe400a2abca2171ce0e63c31babe8
Dirk Hohndel <dirk@hohndel.org>
2023-04-26 03:44:56 +0000

    simplify the interaction between boostrap and setup service
    Instead of looking for a magic file to appear, simply have this service
    killed once the setup restarts docker.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/bootstrap.sh
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/bootstrap.sh
@@ -8,19 +8,8 @@ docker pull ghcr.io/sdr-enthusiasts/docker-adsb-ultrafeeder > docker-pull.log 2>
 # get the local IP address
 IP=$(ip -4 ad li dev $(ip ro li | awk '/default/{ print $5 }') up | awk '/inet/{ print $2 }' | cut -d/ -f1)
 
-skipcount=0
+# this gets stopped and disabled by the setup app
 while true; do
-    skipcount=$((skipcount + 1))
-    if [ "1" = $((skipcount % 12)) ]; then  # about every minute...
-        curl "http://adsb.khh.im/adsb-feeder.html?lip=${IP}" > /dev/null 2>&1
-    fi
-    if [ -f .web-setup.env ] ; then
-        # yay - we have been configured - let's disable this service
-        # and enable the docker app
-        /usr/bin/systemctl disable adsb-bootstrap.service
-        /usr/bin/systemctl enable adsb-docker.service
-        /usr/bin/systemctl start adsb-docker.service
-        break
-    fi
-    sleep 5
+    curl "http://my.adsb.im/adsb-feeder.html?lip=${IP}" > /dev/null 2>&1
+    sleep 60
 done
