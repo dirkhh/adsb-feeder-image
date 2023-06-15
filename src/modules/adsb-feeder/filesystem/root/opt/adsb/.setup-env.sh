bcc604abf97086bf78858ac3b2a7ef18cc114829
Dirk Hohndel <dirk@hohndel.org>
2023-06-15 23:46:05 +0000

    initialize the .env file
    The code that updated the UUID hasn't been useful in ages, so let's get
    rid of that. This work is now done as part of the setup tool.
    Instead use this script to set up the initial versions of the docker
    images that we want to use.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/.setup-env.sh
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/.setup-env.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
+#
+# set up the initial .env file to start from
+cp docker.image.versions .env
 
-WORKING_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
-WORKING_DIR="$(cd -- "$WORKING_DIR" && pwd)"
-if grep -q 00000000-0000-0000-0000-000000000000 "$WORKING_DIR"/.env ; then
-	sed -i "s|^.*UUID=00000000-0000-0000-0000-000000000000|UUID=$(cat /proc/sys/kernel/random/uuid)|" "$WORKING_DIR"/.env
-fi
