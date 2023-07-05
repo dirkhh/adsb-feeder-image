86df145ed1dad33fa68abb14662772f0db5d9e56
Dirk Hohndel <dirk@hohndel.org>
2023-07-05 12:11:33 -0700

    fix syntax error that prevented early docker image pull
    This didn't prevent things from working overall, but the pre-population
    of the ultrafeeder docker container wasn't working which resulted in a
    less smooth user experience.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/.setup-env.sh
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/.setup-env.sh
@@ -7,7 +7,8 @@ fi
 
 # pull the Ultrafeeder container... do this in a loop in case networking isn't
 # quite ready, yet.
-while : do
+while :
+do
 	docker pull $(grep "ULTRAFEEDER_CONTAINER=" docker.image.versions | cut -d\   -f 1) && break
 	sleep 5
 done
