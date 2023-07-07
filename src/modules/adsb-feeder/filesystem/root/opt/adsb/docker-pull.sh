9449dc2e458e4613b0787118e9dc839f5edefa43
Dirk Hohndel <dirk@hohndel.org>
2023-07-07 21:51:21 +0000

    only pull the currently selected containers
    It makes no sense to wait for all of them to be pulled, even if they
    aren't used.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/docker-pull.sh
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/docker-pull.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 #
 # pull the currently selected images
+cd /opt/adsb
 for image in $(grep "_CONTAINER=" .env); do
-	docker pull $(echo $image | cut -d= -f2) >> docker-pull.log 2>&1
+	pref=$(echo $image | cut -d_ -f1)
+	if [ "$pref" = "ULTRAFEEDER" ] ; then
+		docker pull $(echo $image | cut -d= -f2) >> docker-pull.log 2>&1
+	else
+		if grep "$pref=1" .env > /dev/null 2>&1 ; then
+			docker pull $(echo $image | cut -d= -f2) >> docker-pull.log 2>&1
+		fi
+	fi
 done
