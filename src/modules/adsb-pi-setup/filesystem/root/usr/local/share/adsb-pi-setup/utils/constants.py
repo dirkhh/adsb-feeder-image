792ece977e84da37d4c5dc09ef31379f10ff51f5
Dirk Hohndel <dirk@hohndel.org>
2023-07-11 16:27:05 -0700

    clean up mandatory and default settings for Env
    Somehow most values ended up with 'True' as default value...
    Let's only explicitly set mandatory to False for those values where the default
    setting of True is incorrect.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- src/modules/adsb-pi-setup/filesystem/root/usr/local/share/adsb-pi-setup/utils/constants.py
+++ src/modules/adsb-pi-setup/filesystem/root/usr/local/share/adsb-pi-setup/utils/constants.py
@@ -86,130 +86,124 @@ class Constants:
     _env = {
         # Mandatory!
         # Position
-        Env("FEEDER_LAT", True, tags=["lat"]),
-        Env("FEEDER_LONG", True, tags=["lng"]),
-        Env("FEEDER_ALT_M", True, tags=["alt"]),
-        Env("FEEDER_TZ", True, tags=["form_timezone"]),
-        Env("MLAT_SITE_NAME", True, tags=["mlat_name"]),
+        Env("FEEDER_LAT", tags=["lat"]),
+        Env("FEEDER_LONG", tags=["lng"]),
+        Env("FEEDER_ALT_M", tags=["alt"]),
+        Env("FEEDER_TZ", tags=["form_timezone"]),
+        Env("MLAT_SITE_NAME", tags=["mlat_name"]),
         # SDR
-        Env("FEEDER_RTL_SDR", True, default="rtlsdr"),
-        Env("FEEDER_ENABLE_BIASTEE", True, default="false"),
-        Env("FEEDER_READSB_GAIN", True, default="autogain"),
-        Env("FEEDER_SERIAL_1090", False, tags=["1090"]),  # FIXME
-        Env("FEEDER_978", False, tags=["978"]),  # FIXME
+        Env("FEEDER_RTL_SDR", default="rtlsdr"),
+        Env("FEEDER_ENABLE_BIASTEE", default="false"),
+        Env("FEEDER_READSB_GAIN", default="autogain"),
+        Env("FEEDER_SERIAL_1090", is_mandatory=False, tags=["1090"]),  # FIXME
+        Env("FEEDER_978", is_mandatory=False, tags=["978"]),  # FIXME
         # Feeder
-        Env("ADSBLOL_UUID", True, default_call=lambda: str(uuid4())),
-        Env("ULTRAFEEDER_UUID", True, default_call=lambda: str(uuid4())),
-        Env("MLAT_PRIVACY", True, default="--privacy", tags=["mlat_privacy"]),
-        Env("FEEDER_TAR1090_USEROUTEAPI", True, default="1"),
+        Env("ADSBLOL_UUID", default_call=lambda: str(uuid4())),
+        Env("ULTRAFEEDER_UUID", default_call=lambda: str(uuid4())),
+        Env("MLAT_PRIVACY", default="--privacy", tags=["mlat_privacy"]),
+        Env("FEEDER_TAR1090_USEROUTEAPI", default="true"),
         # Misc
         Env(
-            "FEEDER_HEYWHATSTHAT_ID", False, tags=["FEEDER_HEYWHATSTHAT_ID"]
-        ),  # FIXME
-        # Other aggregators keys
-        Env("FEEDER_FR24_SHARING_KEY", False, tags=["fr24", "key"]),
-        Env("FEEDER_PIAWARE_FEEDER_ID", False, tags=["flightaware", "user"]),
-        Env("FEEDER_RADARBOX_SHARING_KEY", False, tags=["radarbox24", "key"]),
-        Env("FEEDER_PLANEFINDER_SHARECODE", False, tags=["planefinder", "key"]),
-        Env("FEEDER_ADSBHUB_STATION_KEY", False, tags=["adsb_hub", "key"]),
-        Env("FEEDER_OPENSKY_USERNAME", False, tags=["opensky", "user"]),
-        Env("FEEDER_OPENSKY_SERIAL", False, tags=["opensky", "pass"]),
-        Env("FEEDER_RV_FEEDER_KEY", False, tags=["radar_virtuel", "key"]),
-        Env("FEEDER_PLANEWATCH_API_KEY", False, tags=["plane_watch", "key"]),
-        # ADSB.im specific
-        Env(
-            "_ADSB_IM_AGGREGATORS_SELECTION",
-            True,
-            default="",
-            tags=["aggregators"],
+            "FEEDER_HEYWHATSTHAT_ID", is_mandatory=False, tags=["FEEDER_HEYWHATSTHAT_ID"]
         ),
-        Env("_ADSBIM_BASE_VERSION", False, tags=["base_version"]),
-        Env("_ADSBIM_CONTAINER_VERSION", False, tags=["container_version"]),
+        # Other aggregators keys
+        Env("FEEDER_FR24_SHARING_KEY", is_mandatory=False, tags=["fr24", "key"]),
+        Env("FEEDER_PIAWARE_FEEDER_ID", is_mandatory=False, tags=["flightaware", "user"]),
+        Env("FEEDER_RADARBOX_SHARING_KEY", is_mandatory=False, tags=["radarbox24", "key"]),
+        Env("FEEDER_PLANEFINDER_SHARECODE", is_mandatory=False, tags=["planefinder", "key"]),
+        Env("FEEDER_ADSBHUB_STATION_KEY", is_mandatory=False, tags=["adsb_hub", "key"]),
+        Env("FEEDER_OPENSKY_USERNAME", is_mandatory=False, tags=["opensky", "user"]),
+        Env("FEEDER_OPENSKY_SERIAL", is_mandatory=False, tags=["opensky", "pass"]),
+        Env("FEEDER_RV_FEEDER_KEY", is_mandatory=False, tags=["radar_virtuel", "key"]),
+        Env("FEEDER_PLANEWATCH_API_KEY", is_mandatory=False, tags=["plane_watch", "key"]),
+        # ADSB.im specific
+        Env("_ADSB_IM_AGGREGATORS_SELECTION", tags=["aggregators"]),
+        Env("_ADSBIM_BASE_VERSION", is_mandatory=False, tags=["base_version"]),
+        Env("_ADSBIM_CONTAINER_VERSION", is_mandatory=False, tags=["container_version"]),
         Env(
             "_ADSBIM_STATE_IS_SECURE_IMAGE",
             is_mandatory=False,
-            default=False,
+            default="false",
             tags=["secure_image", "is_enabled"]
         ),
         Env(
             "_ADSBIM_STATE_IS_FLIGHTRADAR24_ENABLED",
-            False,
+            is_mandatory=False,
             tags=["other_aggregator", "is_enabled", "fr24"],
         ),
         Env(
             "_ADSBIM_STATE_IS_PLANEWATCH_ENABLED",
-            False,
+            is_mandatory=False,
             tags=["other_aggregator", "is_enabled", "plane_watch"],
         ),
         Env(
             "_ADSBIM_STATE_IS_FLIGHTAWARE_ENABLED",
-            False,
+            is_mandatory=False,
             tags=["other_aggregator", "is_enabled", "flightaware"],
         ),
         Env(
             "_ADSBIM_STATE_IS_RADARBOX24_ENABLED",
-            False,
+            is_mandatory=False,
             tags=["other_aggregator", "is_enabled", "radarbox24"],
         ),
         Env(
             "_ADSBIM_STATE_IS_PLANEFINDER_ENABLED",
-            False,
+            is_mandatory=False,
             tags=["other_aggregator", "is_enabled", "planefinder"],
         ),
         Env(
             "_ADSBIM_STATE_IS_ADSBHUB_ENABLED",
-            False,
+            is_mandatory=False,
             tags=["other_aggregator", "is_enabled", "adsb_hub"],
         ),
         Env(
             "_ADSBIM_STATE_IS_OPENSKY_ENABLED",
-            False,
+            is_mandatory=False,
             tags=["other_aggregator", "is_enabled", "opensky"],
         ),
         Env(
             "_ADSBIM_STATE_IS_RADARVIRTUEL_ENABLED",
-            False,
+            is_mandatory=False,
             tags=["other_aggregator", "is_enabled", "radar_virtuel"],
         ),
-        Env("_ADSBIM_STATE_IS_AIRSPY_ENABLED", False, tags=["airspy", "enabled"]),
-        Env("_ADSBIM_STATE_IS_PORTAINER_ENABLED", False, tags=["portainer", "enabled"]),
+        Env("_ADSBIM_STATE_IS_AIRSPY_ENABLED", is_mandatory=False, tags=["airspy", "enabled"]),
+        Env("_ADSBIM_STATE_IS_PORTAINER_ENABLED", is_mandatory=False, tags=["portainer", "enabled"]),
         Env(
             "_ADSBIM_STATE_IS_BASE_CONFIG_FINISHED",
-            True,
             default="0",
             tags=["base_config", "finished"],
         ),
         Env(
             "_ADSBIM_STATE_IS_NIGHTLY_BASE_UPDATE_ENABLED",
-            False,
+            is_mandatory=False,
             tags=["nightly_base_update", "is_enabled"],
         ),Env(
             "_ADSBIM_STATE_IS_NIGHTLY_FEEDER_UPDATE_ENABLED",
-            False,
+            is_mandatory=False,
             tags=["nightly_feeder_update", "is_enabled"],
         ),
         Env(
             "_ADSBIM_STATE_IS_NIGHTLY_CONTAINER_UPDATE_ENABLED",
-            False,
+            is_mandatory=False,
             tags=["nightly_container_update", "is_enabled"],
         ),
         Env(
             "_ADSBIM_STATE_ZEROTIER_KEY",
-            False,
+            is_mandatory=False,
             tags=["zerotierid", "key"],
         ),
         # Other aggregator images
-        Env("_ADSBIM_CONTAINER_FR24", True, tags=["fr24", "container"]),
-        Env("_ADSBIM_CONTAINER_FLIGHTAWARE", True, tags=["flightaware", "container"]),
-        Env("_ADSBIM_CONTAINER_RADARBOX24", True, tags=["radarbox24", "container"]),
-        Env("_ADSBIM_CONTAINER_PLANEFINDER", True, tags=["planefinder", "container"]),
-        Env("_ADSBIM_CONTAINER_ADSBHUB", True, tags=["adsb_hub", "container"]),
-        Env("_ADSBIM_CONTAINER_OPENSKY", True, tags=["opensky", "container"]),
+        Env("_ADSBIM_CONTAINER_FR24", tags=["fr24", "container"]),
+        Env("_ADSBIM_CONTAINER_FLIGHTAWARE", tags=["flightaware", "container"]),
+        Env("_ADSBIM_CONTAINER_RADARBOX24", tags=["radarbox24", "container"]),
+        Env("_ADSBIM_CONTAINER_PLANEFINDER", tags=["planefinder", "container"]),
+        Env("_ADSBIM_CONTAINER_ADSBHUB", tags=["adsb_hub", "container"]),
+        Env("_ADSBIM_CONTAINER_OPENSKY", tags=["opensky", "container"]),
         Env(
-            "_ADSBIM_CONTAINER_RADARVIRTUEL", True, tags=["radar_virtuel", "container"]
+            "_ADSBIM_CONTAINER_RADARVIRTUEL", tags=["radar_virtuel", "container"]
         ),
-        Env("_ADSBIM_CONTAINER_ULTRAFEEDER", True, tags=["ultrafeeder", "container"]),
-        Env("_ADSBIM_CONTAINER_PLANEWATCH", True, tags=["plane_watch", "container"]),
+        Env("_ADSBIM_CONTAINER_ULTRAFEEDER", tags=["ultrafeeder", "container"]),
+        Env("_ADSBIM_CONTAINER_PLANEWATCH", tags=["plane_watch", "container"]),
         # Ultrafeeder config
         Env(
             "_ADSBIM_STATE_IS_ULTRAFEEDER_ADSBLOL_ENABLED",
@@ -232,7 +226,7 @@ class Constants:
             tags=["tat", "ultrafeeder", "is_enabled"],
         ),
         Env(
-            "_ADSBIM_STATE_IS_ULTRAFEEDER_PS_ENABLED", False, tags=["ps", "ultrafeeder"]
+            "_ADSBIM_STATE_IS_ULTRAFEEDER_PS_ENABLED", is_mandatory=False, tags=["ps", "ultrafeeder"]
         ),
         Env(
             "_ADSBIM_STATE_IS_ULTRAFEEDER_ADSBONE_ENABLED",
