b88af975077161c8fb15003c6976c4600a472e5e
Dirk Hohndel <dirk@hohndel.org>
2023-07-16 16:49:13 -0700

    correctly set up the ultrafeeders in the three scenarios
    Depending on the selection for the aggregators on the setup screen either allow
    all of them, the ones with a privacy policy, or simply make this an individual
    choice.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- src/modules/adsb-pi-setup/filesystem/root/usr/local/share/adsb-pi-setup/utils/netconfig.py
+++ src/modules/adsb-pi-setup/filesystem/root/usr/local/share/adsb-pi-setup/utils/netconfig.py
@@ -1,11 +1,8 @@
-def print_err(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
-
 class NetConfig:
     def __init__(self, adsb_config: str, mlat_config: str, has_policy: bool):
         self.adsb_config = adsb_config
         self.mlat_config = mlat_config
-        self.has_policy = has_policy
+        self._has_policy = has_policy
 
     def generate(self, mlat_privacy: bool = True, uuid: str = None):
         adsb_line = self.adsb_config
@@ -19,6 +16,11 @@ class NetConfig:
             mlat_line += ",--privacy"
         return f"{adsb_line};{mlat_line}"
 
+    @property
+    def has_policy(self):
+        return self._has_policy
+
+
 
 class UltrafeederConfig:
     def __init__(self, constants):
@@ -26,23 +28,25 @@ class UltrafeederConfig:
 
     @property
     def _enabled_aggregators(self):
+        aggregator_selection = self._constants.env_by_tags("aggregators").value
         return {
             name: value
             for name, value in self._constants.netconfigs.items()
-            if self._constants.is_enabled("ultrafeeder", name)
+            if (
+                aggregator_selection == "all"
+                or aggregator_selection == "privacy" and self._constants.netconfigs[name].has_policy
+                or aggregator_selection == "individual" and self._constants.is_enabled("ultrafeeder", name)
+            )
         }
 
     def generate(self):
         mlat_privacy = self._constants.is_enabled("mlat_privacy")
         ret = set()
         for name, netconfig in self._enabled_aggregators.items():
-            uuid = self._constants.env_by_tags("ultrafeeder_uuid")
+            uuid = self._constants.env_by_tags("ultrafeeder_uuid").value
             if name == "adsblol":
                 uuid = self._constants.env_by_tags("adsblol_uuid").value
-            crumb = netconfig.generate(mlat_privacy=mlat_privacy, uuid=uuid)
-            if crumb == "":
-                print_err(f"skipping {name} because it is empty?")
-            ret.add(crumb)
+            ret.add(netconfig.generate(mlat_privacy=mlat_privacy, uuid=uuid))
         ret.discard("")
 
         return ";".join(ret)
