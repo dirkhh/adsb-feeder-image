1cb185c1c8280163c2766da343a6a5accab70a53
Dirk Hohndel <dirk@hohndel.org>
2023-07-18 13:20:55 -0700

    adjust docker-pull script
    We are back to 'True' for enabled containers.
    This also adds a '-a' argument that simply gets us all the containers.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/docker-pull.sh
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/docker-pull.sh
@@ -2,12 +2,13 @@
 #
 # pull the currently selected images
 cd /opt/adsb
+[ "$1" = "-a" ] && ALL="1"
 for image in $(grep "_CONTAINER=" .env); do
 	pref=$(echo $image | cut -d_ -f1)
-	if [ "$pref" = "ULTRAFEEDER" ] ; then
+	if [ ! -z $ALL ] || [ "$pref" = "ULTRAFEEDER" ] ; then
 		docker pull $(echo $image | cut -d= -f2) >> docker-pull.log 2>&1
 	else
-		if grep "$pref=1" .adsbim.env > /dev/null 2>&1 ; then
+		if grep "$pref=True" .adsbim.env > /dev/null 2>&1 ; then
 			docker pull $(echo $image | cut -d= -f2) >> docker-pull.log 2>&1
 		fi
 	fi
