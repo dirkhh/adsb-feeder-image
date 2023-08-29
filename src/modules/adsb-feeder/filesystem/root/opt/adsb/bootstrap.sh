ef839c10e6d632550da9da32e66a66ba38e39504
Dirk Hohndel <dirk@hohndel.org>
2023-08-29 16:37:53 -0700

    adjust the bootstrap service to new file locations
    Also make sure that it starts in the background.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/bootstrap.sh
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/bootstrap.sh
@@ -2,9 +2,9 @@
 
 # while the user is getting ready, let's try to pull the ultrafeeder docker
 # container in the background -- that way startup will feel quicker
-cd /opt/adsb
+cd /opt/adsb/config
 if [ ! -f .env ] ; then
-	cp docker.image.versions .env
+	cp /opt/adsb/docker.image.versions .env
 	echo "_ADSBIM_BASE_VERSION=$(cat /opt/adsb/adsb.im.version)" >> .env
 	echo "_ADSBIM_CONTAINER_VERSION=$(cat /opt/adsb/adsb.im.version)" >> .env
 fi
@@ -17,4 +17,4 @@ IP=$(ip -4 ad li dev $(ip ro li | head -1 | awk '/default/{ print $5 }') up | aw
 while true; do
     curl "https://my.adsb.im/adsb-feeder.html?lip=${IP}" > /dev/null 2>&1
     sleep 60
-done
+done &
