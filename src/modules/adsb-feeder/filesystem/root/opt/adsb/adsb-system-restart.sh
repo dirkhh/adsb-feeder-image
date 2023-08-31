dabe29628d3e8ab84a5179b21d9781947b0f5dec
Dirk Hohndel <dirk@hohndel.org>
2023-08-31 11:34:16 -0700

    ADSB system first start is different for the app
    When running as the Feeder Image, there are a few more housekeeping tasks
    (e.g., stopping the bootstrap service).
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/adsb-system-restart.sh
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/adsb-system-restart.sh
@@ -1,7 +1,8 @@
 
 #!/bin/bash
 
-if [ ! -f /opt/adsb/init-complete ] ; then
+if [[ ! -f /opt/adsb/init-complete && -f /boot/ADSB-README.txt ]] ; then
+	# first time we do this on a Feeder Image, we need to do some more housekeeping
 	echo "first time adsb-system-restart: restart docker and stop bootstrap" 1>&2
 	touch /opt/adsb/init-complete
 	/usr/bin/systemctl daemon-reload
@@ -9,6 +10,10 @@ if [ ! -f /opt/adsb/init-complete ] ; then
 	/usr/bin/systemctl disable adsb-bootstrap
 	/usr/bin/systemctl stop adsb-bootstrap
 else
+	# if this isn't the first time we do a restart, or we are running as an app,
+	# things are simpler (we are touching the flag file every time to make this
+	# work both for the Feeder Image and when used as an app)
+	touch /opt/adsb/init-complete
 	echo "adsb-system-restart: trigger docker compose up" 1>&2
 	/opt/adsb/docker-compose-start
 fi
