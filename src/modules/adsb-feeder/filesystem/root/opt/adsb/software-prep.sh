0790f3ca13fbff2d8f0bb1c9ab01546e350fd5ed
Dirk Hohndel <dirk@hohndel.org>
2023-09-06 21:08:11 -0700

    avoid race condition when services are started
    We want the non-image service to set things up for the 'app' case,
    before the adsb-setup can start.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/software-prep.sh
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/software-prep.sh
@@ -7,16 +7,15 @@ touch /opt/adsb/app.adsb.feeder.image
 # while the user is getting ready, let's try to pull the ultrafeeder docker
 # container in the background -- that way startup will feel quicker
 cd /opt/adsb/config
-if [ ! -f .env ] ; then
-	cp /opt/adsb/docker.image.versions .env
-	echo "_ADSBIM_BASE_VERSION=$(cat /opt/adsb/adsb.im.version)" >> .env
-	echo "_ADSBIM_CONTAINER_VERSION=$(cat /opt/adsb/adsb.im.version)" >> .env
-	echo "_ADSBIM_STATE_WEBPORT=1090" >> .env
-fi
+cat /opt/adsb/docker.image.versions >> .env
+echo "_ADSBIM_BASE_VERSION=$(cat /opt/adsb/adsb.im.version)" >> .env
+echo "_ADSBIM_CONTAINER_VERSION=$(cat /opt/adsb/adsb.im.version)" >> .env
+echo "_ADSBIM_STATE_WEBPORT=1090" >> .env
+
 if [ ! -f /opt/adsb/feeder-image.name ] ; then
 	echo "ADSB Feeder app" > /opt/adsb/feeder-image-name
 fi
-bash docker-pull.sh &
+bash /opt/adsb/docker-pull.sh &
 
 # this version does not enable the my.adsb.im trick as it will be running on
 # a different port
