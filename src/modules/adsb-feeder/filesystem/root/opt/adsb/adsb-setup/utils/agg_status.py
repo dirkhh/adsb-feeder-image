ec1c3bb656d1bdaea6d906f55ddb4167afdcf33c
Dirk Hohndel <dirk@hohndel.org>
2023-11-05 09:23:40 -0800

    make json parsing more robust
    Instead of blindly accessing data in the json make sure that the elements we
    are trying to access actually exist.
    
    Fixes: #82
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/adsb-setup/utils/agg_status.py
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/adsb-setup/utils/agg_status.py
@@ -105,73 +105,105 @@ class AggStatus:
             json_url = "https://api.adsb.lol/0/me"
             response_dict = self.get_json(json_url)
             if response_dict:
-                self._beast = T.Yes if len(response_dict["clients"]["beast"]) else T.No
-                self._mlat = T.Yes if len(response_dict["clients"]["mlat"]) else T.No
-                self._last_check = datetime.now()
+                lolclients = response_dict.get("clients")
+                if lolclients:
+                    lolbeast = lolclients.get("beast")
+                    lolmlat = lolclients.get("mlat")
+                    self._beast = (
+                        T.Yes if isinstance(lolbeast, list) and len(lolbeast) else T.No
+                    )
+                    self._mlat = (
+                        T.Yes if isinstance(lolmlat, list) and len(lolmlat) else T.No
+                    )
+                    self._last_check = datetime.now()
         if self._agg == "flyitaly":
             # get the data from json
             json_url = "https://my.flyitalyadsb.com/am_i_feeding"
             response_dict = self.get_json(json_url)
             if response_dict:
                 feeding = response_dict["feeding"]
-                self._beast = T.Yes if feeding["beast"] else T.No
-                self._mlat = T.Yes if feeding["mlat"] else T.No
-                self._last_check = datetime.now()
+                if feeding:
+                    self._beast = T.Yes if feeding.get("beast") else T.No
+                    self._mlat = T.Yes if feeding.get("mlat") else T.No
+                    self._last_check = datetime.now()
         elif self._agg == "adsbfi":
             # get the data from json
             json_url = "https://api.adsb.fi/v1/myip"
             adsbfi_dict = self.get_json(json_url)
             if adsbfi_dict:
                 # print_err(f"adsbfi returned {adsbfi_dict}")
-                self._beast = T.No if adsbfi_dict["beast"] == [] else T.Yes
-                self._mlat = T.No if adsbfi_dict["mlat"] == [] else T.Yes
+                self._beast = T.No if adsbfi_dict.get("beast") == [] else T.Yes
+                self._mlat = T.No if adsbfi_dict.get("mlat") == [] else T.Yes
                 self._last_check = datetime.now()
         elif self._agg == "radarplane":
             json_url = "https://radarplane.com/api/v1/feed/check"
             radarplane_dict = self.get_json(json_url)
             if radarplane_dict:
-                # print_err(f"radarplane returned {radarplane_dict}")
-                self._beast = T.Yes if radarplane_dict["data"]["beast"] else T.No
-                self._mlat = T.Yes if radarplane_dict["data"]["mlat"] else T.No
-                self._last_check = datetime.now()
+                rdata = radarplane_dict.get("data")
+                if rdata:
+                    self._beast = T.Yes if rdata.get("beast") else T.No
+                    self._mlat = T.Yes if rdata.get("mlat") else T.No
+                    self._last_check = datetime.now()
         elif self._agg == "flightaware":
             json_url = f"{self._url}/fa-status.json/"
             fa_dict = self.get_json(json_url)
             if fa_dict:
                 # print_err(f"fa status.json returned {fa_dict}")
-                self._beast = T.Yes if fa_dict["adept"]["status"] == "green" else T.No
-                self._mlat = T.Yes if fa_dict["mlat"]["status"] == "green" else T.No
+                self._beast = (
+                    T.Yes
+                    if fa_dict.get("adept")
+                    and fa_dict.get("adept").get("status") == "green"
+                    else T.No
+                )
+                self._mlat = (
+                    T.Yes
+                    if fa_dict.get("mlat")
+                    and fa_dict.get("mlat").get("status") == "green"
+                    else T.No
+                )
                 self._last_check = datetime.now()
         elif self._agg == "flightradar":
             json_url = f"{self._url}/fr24-monitor.json"
             fr_dict = self.get_json(json_url)
             if fr_dict:
                 # print_err(f"fr monitor.json returned {fr_dict}")
-                self._beast = T.Yes if fr_dict["feed_status"] == "connected" else T.No
+                self._beast = (
+                    T.Yes if fr_dict.get("feed_status") == "connected" else T.No
+                )
                 self._last_check = datetime.now()
         elif self._agg == "radarplane":
             uuid = self._constants.env_by_tags("ultrafeeder_uuid").value
             json_url = f"https://radarplane.com/api/v1/feed/check/{uuid}"
             rp_dict = self.get_json(json_url)
-            if rp_dict:
-                self._beast = T.Yes if rp_dict["data"]["beast"] else T.No
-                self._mlat = T.Yes if rp_dict["data"]["mlat"] else T.No
+            if rp_dict and rp_dict.get("data"):
+                self._beast = T.Yes if rp_dict["data"].get("beast") else T.No
+                self._mlat = T.Yes if rp_dict["data"].get("mlat") else T.No
                 self._last_check = datetime.now()
         elif self._agg == "alive":
             json_url = "https://api.airplanes.live/feed-status"
             a_dict = self.get_json(json_url)
             if a_dict:
                 uuid = self._constants.env_by_tags("ultrafeeder_uuid").value
-                self._beast = (
-                    T.Yes
-                    if any({bc["rId"] == uuid for bc in a_dict["beast_clients"]})
-                    else T.No
-                )
-                self._mlat = (
-                    T.Yes
-                    if any({mc["uuid"][0] == uuid for mc in a_dict["mlat_clients"]})
-                    else T.No
-                )
+                beast_clients = a_dict.get("beast_clients")
+                if beast_clients:
+                    self._beast = (
+                        T.Yes
+                        if any({bc.get("rId") == uuid for bc in beast_clients})
+                        else T.No
+                    )
+                mlat_clients = a_dict.get("mlat_clients")
+                if mlat_clients:
+                    self._mlat = (
+                        T.Yes
+                        if any(
+                            {
+                                isinstance(mc.get("uuid"), list)
+                                and mc.get("uuid")[0] == uuid
+                                for mc in mlat_clients
+                            }
+                        )
+                        else T.No
+                    )
                 self._last_check = datetime.now()
         elif self._agg == "adsbx":
             html_url = "https://www.adsbexchange.com/myip/"
