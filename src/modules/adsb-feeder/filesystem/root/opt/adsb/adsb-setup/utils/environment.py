989d2f726c1ac484e1ec3c89da0f1738fc151e52
Dirk Hohndel <dirk@hohndel.org>
2024-04-07 12:01:27 -0700

    improve import of older .env files
    This only matters if upgrading pre-1.3 feeder images.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/adsb-setup/utils/environment.py
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/adsb-setup/utils/environment.py
@@ -39,14 +39,17 @@ class Env:
                 if type(self._default) == bool:
                     self._value = is_true(value_in_file)
                     return
-                if (
-                    type(self._default) == list
-                    and len(self._default) > 0
-                    and type(self._default[0]) == type(value_in_file)
-                ):
-                    self._value = [value_in_file]
-                    print_err(f"converting {self._name} to list {self._value}")
-                    return
+                if type(self._default) == list and len(self._default) > 0:
+                    if type(self._default[0]) == type(value_in_file):
+                        self._value = [value_in_file]
+                        print_err(f"converting {self._name} to list {self._value}")
+                        return
+                    if type(self._default[0]) == bool and (
+                        value_in_file.lower() in ["true", "false", "0", "1"]
+                    ):
+                        self._value = [is_true(value_in_file)]
+                        print_err(f"converting {self._name} to list {self._value}")
+                        return
                 if type(self._default) == int and type(value_in_file) == str:
                     try:
                         self._value = int(value_in_file)
