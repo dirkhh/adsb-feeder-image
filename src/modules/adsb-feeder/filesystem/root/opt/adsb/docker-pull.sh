4932d8d57194c4bf550b0ee31598d05ee9426aab
Matthias Wirth <matthias.wirth@gmail.com>
2024-05-06 15:30:17 +0200

    docker-pull.sh less verbose log directly to adsb-setup.log

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/docker-pull.sh
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/docker-pull.sh
@@ -11,18 +11,23 @@ fi
 cd /opt/adsb/config
 [ "$1" = "-a" ] && ALL="1"
 
+function pull() {
+    echo -n "$(date -u +"%FT%T.%3NZ") docker pull $@"
+    docker pull -q "$@" 2>&1
+}
+
 # first get the two we always need:
-docker pull amir20/dozzle:latest >> docker-pull.log 2>&1
-docker pull alpine:latest >> docker-pull.log 2>&1
+pull amir20/dozzle:latest
+pull alpine:latest
 
 # then check if any other are enabled (but always get ULTRAFEEDER)
 for image in $(grep "_CONTAINER=" .env); do
 	pref=$(echo $image | cut -d_ -f1)
 	if [ ! -z $ALL ] || [ "$pref" = "ULTRAFEEDER" ] ; then
-		docker pull $(echo $image | cut -d= -f2) >> docker-pull.log 2>&1
+		pull $(echo $image | cut -d= -f2)
 	else
 		if grep "$pref=True" .env > /dev/null 2>&1 ; then
-			docker pull $(echo $image | cut -d= -f2) >> docker-pull.log 2>&1
+			pull $(echo $image | cut -d= -f2)
 		fi
 	fi
 done
