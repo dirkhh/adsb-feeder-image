44b83a9db848898f6c44f9c2dda7a81f2d8bf1d2
Dirk Hohndel <dirk@hohndel.org>
2024-05-10 19:57:20 -0700

    when updating docker image versions use latest-build tags
    Since we switched to using the latest-build-nnn tags for the SDR-E docker
    images, the update-docker-images-to-latest script didn't work anymore.
    This updates the script to also be based on those tags.
    
    Finally, use the script to pick up the latest container builds.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/update-docker-images-to-latest.sh
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/update-docker-images-to-latest.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 #
-# update the docker images to latest instead of the pinned versions
-
-# this needs to run as root
-if [ $(id -u) != "0" ] ; then
-	echo "this command requires superuser privileges - please run as sudo bash $0"
-	exit 1
-fi
+# update the docker images to the latest "latest-build-xxx" instead of the pinned versions
 
 NOW=$(date -Iseconds)
-cd /opt/adsb
+cd /opt/adsb || { echo "can't cd to /opt/adsb"; exit 1; }
 cp docker.image.versions "docker.image.versions.${NOW}"
-cd /opt/adsb/config
+cd /opt/adsb/config || { echo "can't cd to /opt/adsb/config"; exit 1; }
 cp .env "env.${NOW}"
-for container_line in $(grep "_CONTAINER=" /opt/adsb/config/.env); do
-	image=$(echo $container_line | cut -d= -f2)
-	latest="$(echo $image | cut -d@ -f1):latest"
-	docker pull $latest >> docker-pull.log 2>&1
-	new_container=$(docker inspect --format='{{.RepoDigests}}' $latest)
-	new_container_line="${new_container:1:-1}"
-	echo "->  $new_container_line"
-	sed -i "s|${image}|${new_container_line}|" /opt/adsb/config/.env
-	sed -i "s|${image}|${new_container_line}|" /opt/adsb/docker.image.versions
-done
 
+for container_line in $(grep "_CONTAINER=" /opt/adsb/config/.env); do
+	image=$(echo $container_line | cut -d= -f2 | cut -d: -f1)
+	(echo "$image" | {
+		IFS='/' read -r x user container
+		if [[ $user == "sdr-enthusiasts" ]] ; then
+			package_url="https://github.com/${user}/${container}/pkgs/container/{$container}/versions?filters%5Bversion_type%5D=tagged"
+			latestbuild=$(curl -s "$package_url" | grep latest-build- | sed -E 's/.*tag=(latest-build-[0-9]+)".*/\1/' | sort | tail -1)
+			sed -i "s|${image}|ghcr.io/${user}/${container}:${latestbuild}|" /opt/adsb/config/.env
+			sed -i "s|${image}.*|ghcr.io/${user}/${container}:${latestbuild}|" /opt/adsb/docker.image.versions
+			echo "ghcr.io/sdr-enthusiasts/$container:$latestbuild"
+		fi
+	})
+done
