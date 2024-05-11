ff3dcfbbf36503235a040fbeeaf3c7a0298745ab
Dirk Hohndel <dirk@hohndel.org>
2024-05-10 19:15:49 -0700

    simply use docker compose pull to pull images
    Using our wrapper script we automatically will select all the containers that
    are needed - this is much better than the painful manual approach that actually
    had been broken with v2.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/docker-pull.sh
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/docker-pull.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# pull the currently selected images
+# pull the currently selected images -- simply a convenience wrapper
 
 # this needs to run as root
 if [ $(id -u) != "0" ] ; then
@@ -8,26 +8,4 @@ if [ $(id -u) != "0" ] ; then
 	exit 1
 fi
 
-cd /opt/adsb/config
-[ "$1" = "-a" ] && ALL="1"
-
-function pull() {
-    echo -n "$(date -u +"%FT%T.%3NZ") docker pull $@"
-    docker pull -q "$@" 2>&1
-}
-
-# first get the two we always need:
-pull amir20/dozzle:latest
-pull alpine:latest
-
-# then check if any other are enabled (but always get ULTRAFEEDER)
-for image in $(grep "_CONTAINER=" .env); do
-	pref=$(echo $image | cut -d_ -f1)
-	if [ ! -z $ALL ] || [ "$pref" = "ULTRAFEEDER" ] ; then
-		pull $(echo $image | cut -d= -f2)
-	else
-		if grep "$pref=True" .env > /dev/null 2>&1 ; then
-			pull $(echo $image | cut -d= -f2)
-		fi
-	fi
-done
+bash /opt/adsb/docker-compose-adsb pull
