bf468be6f06a2f6dcc44574d3a04ee6432df940d
Dirk Hohndel <dirk@hohndel.org>
2024-05-26 19:27:03 -0700

    waiting app: show the log while waiting
    This modifies the waiting app to auto scroll a log file while waiting to be
    done with the current task.
    
    This can be used as a placeholder website while doing a feeder upgrade and even
    during the initial boot in order to give the user a response when they access
    the feeder with a web browser.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/adsb-setup/waiting-app.py
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/adsb-setup/waiting-app.py
@@ -1,17 +1,48 @@
-from flask import Flask
+from flask import Flask, Response, render_template
 from sys import argv
+import time
 
 app = Flask(__name__)
+logfile = "/opt/adsb/adsb-setup.log"
+action = "Restarting the"
+
+
+@app.route("/stream-log")
+def stream_log():
+    file = open(logfile, "r")
+
+    def tail():
+        line = ""
+        while True:
+            tmp = file.readline()
+            if tmp:
+                line += tmp
+                if line.endswith("\n"):
+                    yield f"{line}\n\n"
+                    line = "data:"
+            else:
+                time.sleep(0.2)
+
+    return Response(tail(), mimetype="text/event-stream")
+
+
+@app.route("/restarting")
+def restarting():
+    return "restarting"
 
 
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
-def catch_all(path):
-    return "restarting"
+def waiting(path):
+    return render_template("waiting.html", action=action)
 
 
 if __name__ == "__main__":
     port = 80
-    if len(argv) == 2:
+    if len(argv) >= 2:
         port = int(argv[1])
+    if len(argv) >= 3:
+        logfile = argv[2]
+    if len(argv) >= 4:
+        action = argv[3]
     app.run(host="0.0.0.0", port=port)
