2206590ced3264383f7eaca28eebc986372155d8
Matthias Wirth <matthias.wirth@gmail.com>
2024-06-16 10:12:26 +0200

    make docker pull not discard existing progress after first boot
    achieve this by using docker-compose-adsb pull which will has a lock
    that prevents it from running more than once
    use systemd-run instead of starting in bash background to avoid the pull
    in progress being interrupted when the adsb-bootstrap service is stopped

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/docker-pull.sh
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/docker-pull.sh
@@ -8,14 +8,5 @@ if [ $(id -u) != "0" ] ; then
     exit 1
 fi
 
-if ! docker images | grep -qs docker-adsb-ultrafeeder; then
-    # pull ultrafeeder if we have no ultrafeeder version yet
-    # this is just for the first boot while the user configures the basics
-    source /opt/adsb/docker.image.versions
-    for i in {1..3}; do
-        docker pull "$ULTRAFEEDER_CONTAINER" && break
-    done
-fi
-
-# do the regular docker compose pull on all activated containers
-bash /opt/adsb/docker-compose-adsb pull
+# docker compose pull on all activated containers (and ultrafeeder even if not configured yet)
+bash /opt/adsb/docker-compose-adsb pull &>>/opt/adsb/adsb-setup.log
