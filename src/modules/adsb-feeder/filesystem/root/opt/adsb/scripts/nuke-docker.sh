efc31d1cd182c438735df1f0fa40af479991f6c2
Matthias Wirth <matthias.wirth@gmail.com>
2024-06-19 13:19:49 +0200

    add simple script to nuke docker

---
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/nuke-docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+systemctl stop adsb-docker docker docker.socket
+rm -rf /mnt/dietpi_userdata/docker-data /var/lib/docker
+systemctl restart docker docker.socket adsb-docker
+
+
+echo "Done."
+echo "If you want to view the progress of container recreation, use this command:"
+echo "tail -f /opt/adsb/adsb-setup.log"
