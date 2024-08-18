81fe158e365deb7dc877ac733429d65a33a2beca
Matthias Wirth <matthias.wirth@gmail.com>
2024-08-18 23:00:53 +0200

    journald volatile: set max /run usage to 10M

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/journal-set-volatile.sh
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/journal-set-volatile.sh
@@ -1,4 +1,5 @@
 systemctl stop systemd-journald
 rm -rf /var/log/journal
 sed -i -e 's/.*Storage=.*/Storage=volatile/' "/etc/systemd/journald.conf"
+sed -i -e 's/.*RuntimeMaxUse=.*/RuntimeMaxUse=10M/' "/etc/systemd/journald.conf"
 systemctl restart systemd-journald
