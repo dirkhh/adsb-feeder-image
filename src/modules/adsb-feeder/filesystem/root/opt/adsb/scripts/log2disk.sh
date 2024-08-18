6da96ce6279b0e7dcc7862f35455ae70c691b3e4
Matthias Wirth <matthias.wirth@gmail.com>
2024-08-18 12:04:06 +0200

    write log 2 disk on shutdown
    or when adsb-docker service is stopped which usually only happens on
    shutdown

---
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/log2disk.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ -f /run/adsb-feeder-image.log ]]; then
+    TIMESTAMP=$(date +%Y-%m-%d+%H:%M)
+    mkdir -p /opt/adsb/logs
+    zstd /run/adsb-feeder-image.log -o /opt/adsb/logs/adsb-setup.log."$TIMESTAMP".zst
+    truncate -s 0 /run/adsb-feeder-image.log
+    find /opt/adsb/logs -name adsb-setup.log.\* -ctime +7 | xargs rm -f
+fi
