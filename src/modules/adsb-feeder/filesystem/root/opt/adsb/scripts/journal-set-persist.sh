c7de780d8f1143fd2f4a1eac953348a474595285
Matthias Wirth <matthias.wirth@gmail.com>
2024-08-19 09:48:02 +0200

    preserve journal when going from persistent to volatile and back

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/journal-set-persist.sh
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/journal-set-persist.sh
@@ -1,4 +1,16 @@
-systemctl stop systemd-journald
-rm -rf /var/log/journal
+#!/bin/bash
+
+if grep -qs -e '^Storage=persistent' /etc/systemd/journald.conf; then
+    echo set-persistent.sh: already persistent, no actions performed
+    exit 0
+fi
+
+# move over the existing log, if it doesn't work so be it
+mkdir -p /var/log/journal
+cp -f -a "/run/log/journal/$(cat /etc/machine-id)" /var/log/journal/
+
 sed -i -e 's/.*Storage=.*/Storage=persistent/' "/etc/systemd/journald.conf"
+
 systemctl restart systemd-journald
+
+rm -rf /run/log/journal
--- src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/journal-set-volatile.sh
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/journal-set-volatile.sh
@@ -1,5 +1,19 @@
-systemctl stop systemd-journald
-rm -rf /var/log/journal
+#!/bin/bash
+
+if grep -qs -e '^Storage=volatile' /etc/systemd/journald.conf; then
+    echo set-volatile.sh: already volatile, no actions performed
+    exit 0
+fi
+
+# move over the existing log, if it doesn't work so be it
+mkdir -p /run/log/journal
+if [[ $(du -s "/var/log/journal/$(cat /etc/machine-id)" | cut -f1) < 20000 ]]; then
+    cp -f -a "/var/log/journal/$(cat /etc/machine-id)" /run/log/journal/
+fi
+
 sed -i -e 's/.*Storage=.*/Storage=volatile/' "/etc/systemd/journald.conf"
 sed -i -e 's/.*RuntimeMaxUse=.*/RuntimeMaxUse=10M/' "/etc/systemd/journald.conf"
+
 systemctl restart systemd-journald
+
+rm -rf /var/log/journal
