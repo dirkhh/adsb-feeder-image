5feb23946998aa4c878fbd2a3803d7707739d2f4
Matthias Wirth <matthias.wirth@gmail.com>
2024-09-22 10:18:17 +0200

    make zram-swap script more reliable

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/zram-swap.sh
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/zram-swap.sh
@@ -8,47 +8,55 @@ if ! [[ -f /opt/adsb/os.adsb.feeder.image ]]; then
     exit 0
 fi
 
-# probably always good to set unless swap is spinning disk backed
-echo 0 > /proc/sys/vm/page-cluster
+NAME=zram0
+DEV=/dev/$NAME
 
-if [[ -e /dev/zram0 ]]; then
-    exit 1
+if { mount; cat /proc/swaps; } | grep -qs "$DEV"; then
+    echo "zram-swap.sh: $DEV is already mounted or used as swap, no actions performed."
+    exit 0
 fi
 
 modprobe zram
+
+# reset the device to ensure we can set the parameters
+echo 1 > "/sys/block/$NAME/reset"
+
+# https://github.com/lz4/lz4?tab=readme-ov-file#benchmarks
+# Core i7-9700K single thread
 # Compressor            Ratio   Compression Decompression
 # LZ4 default (v1.9.0)  2.101   780 MB/s    4970 MB/s
 # Zstandard 1.4.0 -1    2.883   515 MB/s    1380 MB/s
-echo zstd > /sys/block/zram0/comp_algorithm
+echo zstd > "/sys/block/$NAME/comp_algorithm"
 
-# user max quarter of memory
+# use 1/4 of memory
 use=$(( $(grep -e MemTotal /proc/meminfo | tr -s ' ' | cut -d' ' -f2) / 4 ))
-# never more than 1G
-max=$(( 1 * 1024 * 1024 ))
-# always use at least 256M as zram memory limit
+
+# use at least 256M for systems with small memory
 min=$(( 256 * 1024 ))
 
 if (( use < min )); then
     size=$min
-elif (( use > maxkb )); then
-    size=$max
 else
     size=$use
 fi
 
+# zram maximum memory usage
+echo $(( size ))K > "/sys/block/$NAME/mem_limit"
+
 # disk size
 # let's make this 3x the memory limit in case we get good compression
-echo $(( 4 * size ))K > /sys/block/zram0/disksize
-# max memory usage
-echo $(( size ))K > /sys/block/zram0/mem_limit
+echo $(( 4 * size ))K > "/sys/block/$NAME/disksize"
 
-mkswap /dev/zram0
-swapon -p 100 /dev/zram0
+mkswap "$DEV"
+swapon -p 100 "$DEV"
 
 # turn off file based swap
-swapoff $(cat /proc/swaps | grep -F -v -e zram -e Filename | cut -d' ' -f1) || true
+TURNOFF=$(grep </proc/swaps -F -v -e zram -e Filename | cut -d' ' -f1)
+if [[ -n $TURNOFF ]]; then
+    swapoff $TURNOFF || true
+fi
 
-if ! cat /proc/swaps | grep -qs -F -v -e zram -e Filename; then
+if ! grep </proc/swaps -qs -F -v -e zram -e Filename; then
 
     # for zram swap, most guides reommend the following:
     # swappiness 200, watermark_scale_factor 125, watermark_boost_factor 0
@@ -68,4 +76,7 @@ if ! cat /proc/swaps | grep -qs -F -v -e zram -e Filename; then
     # watermark_boost_factor
     # this has to do with reclaiming on fragmentation, swap on zram guides seem to disable this but don't give a reason
     echo 0 > /proc/sys/vm/watermark_boost_factor
+
+    # disable readahead for reading from swap (default is 3 which means 2^3 = 8 pages)
+    echo 0 > /proc/sys/vm/page-cluster
 fi
