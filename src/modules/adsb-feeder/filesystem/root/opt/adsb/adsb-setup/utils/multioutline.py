6107da33954bc1656542df559e9cb0e2d16addbc
Matthias Wirth <matthias.wirth@gmail.com>
2024-12-04 13:27:06 +0000

    multioutline: improve error logging

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/adsb-setup/utils/multioutline.py
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/adsb-setup/utils/multioutline.py
@@ -3,6 +3,7 @@ import os
 import re
 import subprocess
 import time
+import traceback
 from shapely.geometry import LinearRing, Polygon
 from shapely.ops import unary_union
 from utils.util import make_int, print_err, get_plain_url
@@ -78,6 +79,7 @@ class MultiOutline:
         return result
 
     def create(self, data, hwt_alt=0):
+        #print_err(f"multioutline: called create with for data with len {len(data)}")
         result = {"multiRange": []}
         polygons = []
         for i in range(len(data)):
@@ -86,7 +88,7 @@ class MultiOutline:
                 if d.get("actualRange"):
                     points = d["actualRange"]["last24h"]["points"]
                 else:
-                    print(f"can't get points from outline #{i}: {d}")
+                    print_err(f"multioutline: can't get points from outline #{i}: {d}")
                     points = []
             else:
                 points = [r["points"] for r in d["rings"] if r["alt"] == hwt_alt][0]
@@ -99,16 +101,18 @@ class MultiOutline:
                             if r == "Valid Geometry":
                                 polygons.append(p)
                             else:
-                                print(f"can't create polygon from outline #{i} - {r}")
+                                print_err(f"multioutline: can't create polygon from outline #{i} - {r}")
                         else:
                             try:
                                 polygons.append(p)
                             except:
-                                print(f"can't create polygon from outline #{i}")
+                                print_err(traceback.format_exc())
+                                print_err(f"multioutline: can't create polygon from outline #{i}")
                     else:
-                        print(f"can't create polygon from outline #{i}")
+                        print_err(f"multioutline: can't create polygon from outline #{i}")
                 except:
-                    print(f"can't create linear ring from outline #{i} - maybe there is no data, yet?")
+                    print_err(traceback.format_exc())
+                    print_err(f"multioutline: can't create linear ring from outline #{i} - maybe there is no data, yet?")
         made_change = True
         look_at = range(1, len(polygons))
         while made_change:
@@ -117,11 +121,11 @@ class MultiOutline:
             for i in look_at:
                 combined = False
                 if not polygons[i] or not polygons[i].is_valid:
-                    print(f"polygons[{i}]: {explain_validity(polygons[i])}")
+                    print_err(f"multioutline: polygons[{i}]: {explain_validity(polygons[i])}")
                     polygons[i] = polygons[i].buffer(0.0001)
                 for j in to_consider:
                     if not polygons[j].is_valid:
-                        print(f"polygons[{j}]: {explain_validity(polygons[j])}")
+                        print_err(f"multioutline: polygons[{j}]: {explain_validity(polygons[j])}")
                         polygons[j] = polygons[j].buffer(0.0001)
                     try:
                         if not polygons[j].disjoint(polygons[i]):
@@ -130,7 +134,8 @@ class MultiOutline:
                             made_change = True
                             combined = True
                     except Exception as e:
-                        print(f"exception {e} while combining polygons #{j} and #{i} for hwt_alt={hwt_alt}")
+                        print_err(traceback.format_exc())
+                        print_err(f"multioutline: exception {e} while combining polygons #{j} and #{i} for hwt_alt={hwt_alt}")
                         pass
                 if not combined:
                     to_consider.append(i)
@@ -144,6 +149,7 @@ class MultiOutline:
                     points = [[x, y] for x, y in coords]
                 result["multiRange"].append(points)
             except Exception as e:
-                print_err(f"can't get points from polygon #{i} exterior coords: {e}")
+                print_err(traceback.format_exc())
+                print_err(f"multioutline: can't get points from polygon #{i} exterior coords: {e}")
                 pass
         return result
