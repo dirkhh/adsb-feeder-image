088b4c94010875ee22933f9ae637f328d3094c60
Matthias Wirth <matthias.wirth@gmail.com>
2025-01-08 16:22:34 +0000

    log-sanitizer: make it work with adsb.im state variables
    to do this, move the generation of search replace pairs into the python

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/log-sanitizer.sh
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/log-sanitizer.sh
@@ -105,62 +105,11 @@ $(cat /run/adsb-feeder-image.log 2>&1)
 ${SEPARATOR}
 "
 
-# We set vars to empty
-SANITIZE_VARS="FEEDER_LAT FEEDER_LONG ADSBLOL_UUID AF_MICRO_IP ULTRAFEEDER_UUID FEEDER_1090UK_API_KEY
-ADSBLOL_LINK _ADSBIM_STATE_ALIVE_MAP_LINK _ADSBIM_STATE_ADSBX_FEEDER_ID
-FEEDER_ADSBHUB_STATION_KEY FEEDER_FR24_SHARING_KEY FEEDER_FR24_UAT_SHARING_KEY
-FEEDER_PLANEWATCH_API_KEY FEEDER_RADARBOX_SHARING_KEY FEEDER_RV_FEEDER_KEY
-FEEDER_PIAWARE_FEEDER_ID FEEDER_RADARBOX_SHARING_KEY FEEDER_RADARBOX_SN
-FEEDER_PLANEFINDER_SHARECODE FEEDER_OPENSKY_USERNAME FEEDER_OPENSKY_SERIAL FEEDER_HEYWHATSTHAT_ID"
+# config variable replacement now done in search-replace.py
 
-# We set vars that cannot be empty, have to be stripped
-IMPORTANT_VARS="FEEDER_LAT FEEDER_LONG AF_MICRO_IP"
-
-NUM_MICRO_SITES=$(grep -e "^AF_NUM_MICRO_SITES=" /opt/adsb/config/.env | cut -d'=' -f2)
-
-
-SANITIZE_VARS_ORIG="$SANITIZE_VARS"
-IMPORTANT_VARS_ORIG="$IMPORTANT_VARS"
-
-for i in $(seq $NUM_MICRO_SITES); do
-    for VAR in $SANITIZE_VARS_ORIG; do
-        SANITIZE_VARS+=" ${VAR}_${i}"
-    done
-    for VAR in $IMPORTANT_VARS_ORIG; do
-        IMPORTANT_VARS+=" ${VAR}_${i}"
-    done
-done
-
-# simple fixed string replacement using search-replace.py, search and replace strings are given as pairs of command line arguments
-simple_replace=()
 # regex replacements using perl, this is more consistent than sed due to always different sed versions
 replace_args=()
 
-# For each
-for VAR in $SANITIZE_VARS; do
-  # We get the value of the variable
-  MY_VAR=$(grep -e "^${VAR}=" /opt/adsb/config/.env | sed -e 's/[^=]*=//')
-  # MY_VAR is empty, and it is one of FEEDER_LAT FEEDER_LONG ADSBLOL_UUID, bail out
-  if [ -z "$MY_VAR" ] ; then
-    if [[ "$IMPORTANT_VARS" == *"$VAR"* ]]; then
-      # If we are here, it means that the variable is empty, and it is one of the important ones
-      echo "WARNING: $VAR is empty"
-    fi
-  else
-    #echo "removing all references to ${VAR}"
-    case "$MY_VAR" in
-        None | True | False )
-            continue
-            ;;
-    esac
-    if grep -qs -F -e '$$' <<< "${MY_VAR}"; then
-        # for the .env, $ is replaced with $$, undo this replacement
-        MY_VAR_UNESCAPED="$(sed 's#\$\$#\$#g' <<< "${MY_VAR}")"
-        simple_replace+=("${MY_VAR_UNESCAPED}" "MY_REAL_${VAR}")
-    fi
-    simple_replace+=("${MY_VAR}" "MY_REAL_${VAR}")
-  fi
-done
 # replace --lat --lon arguments mainly from piaware log
 replace_args+='s/--lat.[^ ]*/--lat <redacted>/g;'
 replace_args+='s/--lon.[^ ]*/--lon <redacted>/g;'
@@ -177,4 +126,4 @@ replace_args+='s/((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4]
 # finally, replace everything that looks like a uuid
 replace_args+='s/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/<hidden-uuid>/g;'
 
-perl -pe "${replace_args}" <<< "${SANITIZED_LOG}" | /opt/adsb/scripts/search-replace.py "${simple_replace[@]}"
+perl -pe "${replace_args}" <<< "${SANITIZED_LOG}" | /opt/adsb/scripts/search-replace.py
--- src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/search-replace.py
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/search-replace.py
@@ -1,16 +1,57 @@
 #!/usr/bin/env python3
 
 import sys
+import json
 
-if (len(sys.argv) - 1) % 2 != 0:
-    print("ERROR: even number of search replace pairs required!")
-    sys.exit(1)
+config = json.load(open("/opt/adsb/config/config.json", "r"))
 
-pairs = int((len(sys.argv) - 1) / 2)
 sr = []
-for i in range(pairs):
-    sr.append((sys.argv[2 * i + 1], sys.argv[2 * i + 2]))
-    #print(f"{sr[-1][0]} {sr[-1][1]}")
+
+sanitize_vars = [
+    "FEEDER_LAT",
+    "FEEDER_LONG",
+    "ADSBLOL_UUID",
+    "AF_MICRO_IP",
+    "ULTRAFEEDER_UUID",
+    "FEEDER_1090UK_API_KEY",
+    "ADSBLOL_LINK",
+    "_ADSBIM_STATE_ALIVE_MAP_LINK",
+    "_ADSBIM_STATE_ADSBX_FEEDER_ID",
+    "FEEDER_ADSBHUB_STATION_KEY",
+    "FEEDER_FR24_SHARING_KEY",
+    "FEEDER_FR24_UAT_SHARING_KEY",
+    "FEEDER_PLANEWATCH_API_KEY",
+    "FEEDER_RADARBOX_SHARING_KEY",
+    "FEEDER_RV_FEEDER_KEY",
+    "FEEDER_PIAWARE_FEEDER_ID",
+    "FEEDER_RADARBOX_SHARING_KEY",
+    "FEEDER_RADARBOX_SN",
+    "FEEDER_PLANEFINDER_SHARECODE",
+    "FEEDER_OPENSKY_USERNAME",
+    "FEEDER_OPENSKY_SERIAL",
+    "FEEDER_HEYWHATSTHAT_ID",
+]
+
+for name in sanitize_vars:
+    item = config[name]
+    pairs = []
+    if type(item) == list:
+        count = 0
+        for entry in item:
+            pairs.append((entry, f"{name}_{count}"))
+            count += 1
+    else:
+        pairs.append((item, name))
+
+    for search, replace in pairs:
+        if not search or search == True or search == False:
+            continue
+        search = str(search).strip()
+        if not search:
+            continue
+        #print(f"{search} {replace}")
+        sr.append((search, replace))
+
 
 for line in sys.stdin:
     for search, replace in sr:
