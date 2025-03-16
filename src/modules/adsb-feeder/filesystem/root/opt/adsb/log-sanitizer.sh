83419632989b5b316c3d149c508aedb155080b05
Matthias Wirth <matthias.wirth@gmail.com>
2025-03-16 09:15:09 +0000

    avoid stray curl error message in log sanitizer

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/log-sanitizer.sh
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/log-sanitizer.sh
@@ -25,13 +25,13 @@ ${SEPARATOR}
 "
 
 if ip -6 addr show scope global $(ip -j route get 1.2.3.4 | jq '.[0].dev' -r) | grep -v 'inet6 f' | grep -qs inet6; then
-  if timeout 2 curl -sS -o /dev/null -6 https://google.com; then
+  if err=$(timeout 2 curl -sS -o /dev/null -6 https://google.com 2>&1); then
     SANITIZED_LOG+="IPv6: working"
   else
-    SANITIZED_LOG+="IPv6: BROKEN"
+    SANITIZED_LOG+="IPv6: BROKEN ($err) ($(ip -6 addr show scope global $(ip -j route get 1.2.3.4 | jq '.[0].dev' -r) | grep -v 'inet6 f' | grep inet6))"
   fi
 else
-  if timeout 2 curl -sS -o /dev/null -6 https://google.com; then
+  if err=$(timeout 2 curl -sS -o /dev/null -6 https://google.com 2>&1); then
     SANITIZED_LOG+="IPv6: working (but no global ipv6 address on primary route interface found)"
   else
     SANITIZED_LOG+="IPv6: no global address or disabled"
