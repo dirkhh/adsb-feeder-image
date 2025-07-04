3fba91b7dc12fdf8262273bd6093931a59dcc214
Matthias Wirth <matthias.wirth@gmail.com>
2025-07-04 08:41:45 +0000

    netdog: verbosely state which network state succeeded

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/netdog.sh
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/netdog.sh
@@ -3,12 +3,15 @@
 function test_network() {
     gateway="$(ip route get 1.2.3.4 | awk '/via/ { print $3 }')"
     if ping -i 0 -c2 -w5 "${gateway}" &>/dev/null; then
+        echo "gateway reachable via ping: ${gateway}"
         return 0
     fi
     if ping -c2 -w5 8.8.8.8 &>/dev/null; then
+        echo "8.8.8.8 reachable via ping, ${gateway} not reachable via ping"
         return 0
     fi
     if curl --max-time 5 akamai.com &>/dev/null; then
+        echo "akamai.com reachable via curl, 8.8.8.8 not reachable via ping, ${gateway} not reachable via ping"
         return 0
     fi
     return 1
