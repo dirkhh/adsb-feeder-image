f0db7d0fd744933df255ad6e99f2b533d68faf7d
Matthias Wirth <matthias.wirth@gmail.com>
2025-11-06 04:36:06 +0000

    measure and log multioutline cpu time

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/adsb-setup/push_multioutline.py
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/adsb-setup/push_multioutline.py
@@ -1,12 +1,15 @@
 import json
 import subprocess
 import sys
+import time
 import traceback
 
 from utils.multioutline import MultiOutline
 from utils.util import make_int, print_err
 
 n = make_int(sys.argv[1] if len(sys.argv) > 1 else 1)
+
+t1 = time.process_time()
 
 # multioutline
 try:
@@ -17,6 +20,8 @@ except Exception:
     print_err(traceback.format_exc(), level=8)
     print_err("failed to push multiOutline.json verbose 8 for details")
 
+t2 = time.process_time()
+print_err(f"multioutline generation CPU seconds used: {round(t2 - t1, 3)}")
 
 # heywhatsthat
 
@@ -36,3 +41,6 @@ try:
 except Exception:
     print_err(traceback.format_exc(), level=8)
     print_err("failed to push heywhatsthat.json - verbose 8 for details")
+
+t3 = time.process_time()
+print_err(f"heywhatsthat generation CPU seconds used: {round(t3 - t2, 3)}")
