a84a4b7f63120eeedf318f070c36a88c0312f348
Matthias Wirth <matthias.wirth@gmail.com>
2025-11-11 10:06:47 +0000

    better acarshub DB backup method
    the previous method was really bad, just copying an active DB files is
    going to cause issues at some point

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/disk-to-run.sh
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/disk-to-run.sh
@@ -6,9 +6,17 @@ if cd /run/acars_data; then
     ! [[ -f acarshub.rrd ]] && [[ -f /opt/adsb/config/acarshub/acarshub.rrd.zst ]] \
         && zstd -f --decompress /opt/adsb/config/acarshub/acarshub.rrd.zst -o acarshub.rrd.tmp \
         && mv -f acarshub.rrd.tmp acarshub.rrd
-    ! [[ -f messages.db ]] && [[ -f /opt/adsb/config/acarshub/messages.db.zst ]] \
-        && zstd -f --decompress /opt/adsb/config/acarshub/messages.db.zst -o messages.db.tmp \
-        && mv -f messages.db.tmp messages.db
+    if ! [[ -f messages.db ]]; then
+        if [[ -f /opt/adsb/config/acarshub/messages.db ]]; then
+            # restore current to-disk method of using vacuum into without compression
+            cp /opt/adsb/config/acarshub/messages.db messages.db.tmp \
+                && mv -v -f messages.db.tmp messages.db
+        elif [[ -f /opt/adsb/config/acarshub/messages.db.zst ]]; then
+            # restore old to-disk method of just copying an active DB which was pretty bad really
+            zstd -f --decompress /opt/adsb/config/acarshub/messages.db.zst -o messages.db.tmp \
+                && mv -f messages.db.tmp messages.db
+        fi
+    fi
     # remove temporary files in case the decompression failed half way
     rm -f messages.db.tmp
     rm -f acarshub.rrd.tmp
--- src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/run-to-disk.sh
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/run-to-disk.sh
@@ -4,8 +4,15 @@
 if [[ -d /run/acars_data ]] && mkdir -p /opt/adsb/config/acarshub/ && cd /opt/adsb/config/acarshub; then
     zstd -f --compress /run/acars_data/acarshub.rrd -o acarshub.rrd.zst.tmp \
         && mv -f acarshub.rrd.zst.tmp acarshub.rrd.zst
-    zstd -f --compress /run/acars_data/messages.db -o messages.db.zst.tmp \
-        && mv -f messages.db.zst.tmp messages.db.zst
+    if ! command -v sqlite3 &>/dev/null; then
+        apt update
+        apt install --no-install-recommends --no-install-suggests -y sqlite3
+    fi
+    rm -f messages.db.tmp
+    # backup using vacuum into and delete old backup file
+    sqlite3 /run/acars_data/messages.db "vacuum into 'messages.db.tmp';" \
+        && mv -v -f messages.db.tmp messages.db \
+        && rm -f messages.db.zst
 fi
 
 exit 0
