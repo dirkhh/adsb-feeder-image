062b9b4f02695723425ed0c5bafd6b4624478a89
Matthias Wirth <matthias.wirth@gmail.com>
2026-03-17 20:13:25 +0000

    run-to-disk: adjust for rrd being retired

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/run-to-disk.sh
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/run-to-disk.sh
@@ -2,10 +2,17 @@
 
 # acarshub
 if [[ -d /run/acars_data ]] && mkdir -p /opt/adsb/config/acarshub/ && cd /opt/adsb/config/acarshub; then
+
+    # the rrd file is no longer used by acarshub
+    # write the .back file to disk just in case and ignore it from now on
     if [[ -f /run/acars_data/acarshub.rrd ]]; then
         zstd -f --compress /run/acars_data/acarshub.rrd -o acarshub.rrd.zst.tmp \
             && mv -v -f acarshub.rrd.zst.tmp acarshub.rrd.zst
+    elif [[ -f /run/acars_data/acarshub.rrd.back ]]; then
+        cp /run/acars_data/acarshub.rrd.back /opt/adsb/config/acarshub/
+        rm -f /opt/adsb/config/acarshub/acarshub.rrd.zst
     fi
+
     if ! command -v sqlite3 &>/dev/null; then
         apt update
         apt install --no-install-recommends --no-install-suggests -y sqlite3
