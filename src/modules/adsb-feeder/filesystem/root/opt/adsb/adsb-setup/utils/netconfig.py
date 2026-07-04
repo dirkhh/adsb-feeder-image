27059cfe3e3d393510f75915c5337cb7f5dc7f5c
Matthias Wirth <matthias.wirth@gmail.com>
2026-07-04 14:40:01 +0000

    ultrafeeder extraargs: deal with \r

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/adsb-setup/utils/netconfig.py
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/adsb-setup/utils/netconfig.py
@@ -164,8 +164,8 @@ class UltrafeederConfig:
                 ret.add(f"adsb,dump978,30978,uat_in")
 
         # finally, add user provided things
-        ultrafeeder_extra_args = self._d.env_by_tags("ultrafeeder_extra_args").value.replace("\n", "")
-        ultrafeeder_extra_args_microsites = self._d.env_by_tags("ultrafeeder_extra_args_microsites").value.replace("\n", "")
+        ultrafeeder_extra_args = self._d.env_by_tags("ultrafeeder_extra_args").value.translate(str.maketrans("", "", "\r\n"))
+        ultrafeeder_extra_args_microsites = self._d.env_by_tags("ultrafeeder_extra_args_microsites").value.translate(str.maketrans("", "", "\r\n"))
         if ultrafeeder_extra_args:
             if not is_stage2:
                 ret.add(ultrafeeder_extra_args)
